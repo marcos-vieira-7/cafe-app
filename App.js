d9c731932efffba684fd8515d01f6a2928069e34
Marcos Afonso Vieira <marcos.vieira@pelicano.br>
2024-01-11 10:46:59 -0300

    completo imc

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import Form from './.expo/src/components/form/form';
 import Titulo from './.expo/src/components/title/title';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Titulo />
-
+      <Form />
     </View>
   );
 }
